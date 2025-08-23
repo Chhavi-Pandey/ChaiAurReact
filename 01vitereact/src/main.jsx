bd7fc2e4653e8b8c43c90dd87e5f66a775d79032
Chhavi Pandey <pandeychhavi29@gmail.com>
2025-08-23 19:51:58 +0530

    customReact

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom APP</h1>
+    </div>
+  )
+} 
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google'
+  //you can inject variable in last means here
+)
+
+ReactDOM.createRoot(document.getElementById('root'))
+.render(
+  
+    reactElement
+  
 )
